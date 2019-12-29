4ed0618abbfc6fa967e59a13d5e1494bdbb70b65
Ricky dildy <rdildy92@gmail.com>
2019-12-29 22:08:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,4 +6,14 @@ def actor
    actor = {:name => "Dwayne The Rock Johnson"}
 end
 
-def 
+def monopoly
+  monopoly = {
+    :railroads =>{}
+  }
+  
+end 
+
+
+def monopoly_with_second_tier 
+  monopoly ={ 
+    
