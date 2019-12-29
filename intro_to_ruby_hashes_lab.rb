b3212a188af4ad135211f890d01ae9ace19ece19
Ricky dildy <rdildy92@gmail.com>
2019-12-29 21:55:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,8 +42,8 @@ def monopoly_with_fourth_tier
        :three_pieces_owned => 100,
        :four_pieces_owned => 200 
      }
-     }
      },
+     {
      :names =>
     {
       :reading_railroad =>
@@ -63,4 +63,5 @@ def monopoly_with_fourth_tier
         "mortgage_value" => "$800"
       }
     }
+     }
 end
